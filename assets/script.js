ccdf9a6c1bf0ff1aa2189502953c8ec087092437
OmarAsamarai <oasamarai@gmail.com>
2021-12-26 16:15:20 -0600

    final try

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,57 @@
-// What are the Criterial?
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
 
+// Display the current date in the header
 var currentDate = moment().format("dddd, MMMM Do YYYY")
 $("#currentDay").html(currentDate);
 
+// add a background indicates if the hour past or present or future 
 function timeBlockBg() {
 
     var currentTime = moment().hour();
+    //console.log(currentTime)
 
     $(".time-block").each(function() {
+        var planTime = parseInt($(this).attr("id"));
+            console.log(planTime)
 
-        var planTime= parseInt($(this).attr("id"));
-
-        if (planTime < currentTime) {
-            $(this).addClass("past");
-        } else if (planTime === currentTime) {
-            $(this).addClass("present");
-        } else {
+        if (planTime > currentTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
             $(this).addClass("future");
-        }
-        
+
+        } else if (planTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }       
     })
-
-
-
 };
 
-timeBlockBg()
+// Click save button and to save the plan 
+var saveBtn = $(".saveBtn").on("click", function() {
+    
+    var planText = $(this).siblings(".plan").val();
+    var planTime = $(this).parent().attr("id");
 
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(planTime, planText);
+});
 
-// console.log(currentDate);
-// var row = document.createElement(".rwo")
+// Get the plan text from local storage if refresh happened
+$("#8 .plan").val(localStorage.getItem("8"));
+$("#9 .plan").val(localStorage.getItem("9"));
+$("#10 .plan").val(localStorage.getItem("10"));
+$("#11 .plan").val(localStorage.getItem("11"));
+$("#12 .plan").val(localStorage.getItem("12"));
+$("#13 .plan").val(localStorage.getItem("13"));
+$("#14 .plan").val(localStorage.getItem("14"));
+$("#15 .plan").val(localStorage.getItem("15"));
+$("#16 .plan").val(localStorage.getItem("16"));
+$("#17 .plan").val(localStorage.getItem("17"));
 
-// $(row).createElement
+// Call function
+timeBlockBg();
